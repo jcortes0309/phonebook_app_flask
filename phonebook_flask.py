95250435eda06cc1efbcd8a3e8045c5e3a126a8e
Juan A Cortes <juan.cortes@gmail.com>
2016-10-12 14:21:47 -0400

    Web application renders all the phonebook entries in the phonebook database as an unordered list (ul) at the root URL: /

--- phonebook_flask.py
+++ phonebook_flask.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import pg
 
 db = pg.DB(dbname='phonebook_db')
 # db.debug = True
 
 app = Flask("phonebook")
+
+@app.route("/")
+def contacts():
+    return render_template(
+        "contacts.html",
+        title = "Phonebook Contacts",
+        contact_list = db.query("select * from phonebook").namedresult()
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
